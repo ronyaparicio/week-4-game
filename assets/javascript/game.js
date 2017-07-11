2474a01c957244acad59605d3f7cb776ac1227be
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-07-11 17:40:46 -0600

    coommet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,24 @@
-//generate random number
 $('document').ready(function() {
+  var button1 = 0;
+  var button2 = 0;
+  var button3 = 0;
+  var button4 = 0;
+  var randomNumber =0;
+  var wins = 0;
+  //generate random number and values for buttons
+  function startGame() {
+    button1 = Math.floor((Math.random() * 12) + 1);
+    button2 = Math.floor((Math.random() * 12) + 1);
+    button3 = Math.floor((Math.random() * 12) + 1);
+    button4 = Math.floor((Math.random() * 12) + 1);
+    randomNumber = Math.floor((Math.random()*120)+19);
+  }
+  startGame();
+  //assign values to html
+  document.getElementById('button1') = button1;
+
+
+
 
 
 });
