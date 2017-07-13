99bc1ba79fe38e98f8c252df96a1b00b593a1cff
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-07-12 20:37:03 -0600

    d

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,9 @@ $('document').ready(function() {
   var button2 = 0;
   var button3 = 0;
   var button4 = 0;
-  var randomNumber =0;
+  var randomNumber = 0;
   var wins = 0;
+  var yourNumber = 0
   //generate random number and values for buttons
   function startGame() {
     button1 = Math.floor((Math.random() * 12) + 1);
@@ -12,10 +13,17 @@ $('document').ready(function() {
     button3 = Math.floor((Math.random() * 12) + 1);
     button4 = Math.floor((Math.random() * 12) + 1);
     randomNumber = Math.floor((Math.random()*120)+19);
+    yourNumber = 0;
+
+    $("#randomNumber").html(randomNumber);
+    $("#yourNumber").html(yourNumber)
+
   }
   startGame();
   //assign values to html
-  document.getElementById('button1') = button1;
+
+  $("#button1").val(button1);
+
 
 
 
